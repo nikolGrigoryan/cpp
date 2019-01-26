aa140493125e51441c618f5c5dd4d13fd82bcf0c
Gyumri <user@gyumri>
2019-01-26 11:22:29 +0400

    Added answere to X.7 - 2 assignment.

---
+++ X_7_Types_and_Declarations/nikol_2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+int main()
+{
+				char c = 255;
+				int i = c;
+				cout << "i: " << i << '\n';
+
+				if (numeric_limits<int>::is_signed) {
+								cout << "signed!\n";
+				} else {
+								cout << "unsigned!\n";
+				}
+
+				unsigned char uc = 255;
+				int ui = uc;
+				cout << "ui: " << ui << '\n';
+
+				return 0;
+}
